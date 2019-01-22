08b1207fb7f3696dc1ed29d5ac72b0e82e5effad
wangliang <Liang.SL.Wang@partner.bmw.com>
2019-01-22 11:16:25 +0800

    fix ut

--- fastlaneDemoTests/fastlaneDemoTests.swift
+++ fastlaneDemoTests/fastlaneDemoTests.swift
@@ -26,7 +26,7 @@ class fastlaneDemoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertTrue(flag)
+        XCTAssertTrue(!flag)
         
     }
     
